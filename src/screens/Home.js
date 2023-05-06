a4cb037caab15f5a370256fecb45f626b868ea99
Hamzah Abokreen <hamzah.abokreen@gmail.com>
2023-05-06 14:19:16 +0200

    initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View className="w-28 h-28 bg-red-700 mt-10 justify-center items-center">
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default Home;
